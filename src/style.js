5f41c854e7d4914fab6b299c0951ee7a5e223c84
0xJEth <james.murphy0622@gmail.com>
2016-11-24 15:21:05 -0600

    include fontSize and zIndex

--- src/style.js
+++ src/style.js
@@ -1,11 +1,11 @@
-import { flow, map, merge, method, propertyOf, zipObject } from 'lodash'
+import { flow, map, merge, method, propertyOf, range, zipObject } from 'lodash'
 import { createObj } from 'cape-lodash'
 
 export const rem = flow(String, method('concat', 'rem'))
 
-export const sizeKeys = ['0p5', '1', '2', '3', '4', '5', '6']
-export const sizes = [0.5, 1, 2, 3, 4, 5, 6]
 export function styleBuilder(style, prefix) {
+  const sizeKeys = ['0p5', '1', '2', '3', '4', '5', '6']
+  const sizes = [0.5, 1, 2, 3, 4, 5, 6]
   const sizeBuilder = createObj(style)
   const remBuilder = flow(rem, sizeBuilder)
   return merge(
@@ -14,6 +14,7 @@ export function styleBuilder(style, prefix) {
   )
 }
 const remStyles = {
+  fs: 'fontSize',
   m: 'margin',
   mt: 'marginTop',
   mb: 'marginBottom',
@@ -21,8 +22,22 @@ const remStyles = {
   pt: 'paddingTop',
   pb: 'paddingBottom',
 }
+export function buildNumSizes(style, prefix, start = 0, end = 10) {
+  const sizes = range(start, end)
+  const sizeBuilder = createObj(style)
+  return zipObject(map(sizes, key => prefix + key), map(sizes, sizeBuilder))
+}
 export const styles = {
   ...merge({}, ...map(remStyles, styleBuilder)),
+  static: { position: 'static' },
+  relative: { position: 'relative' },
+  absolute: { position: 'absolute' },
+  fixed: { position: 'fixed' },
+  top0: { top: 0 },
+  right0: { right: 0 },
+  bottom0: { bottom: 0 },
+  left0: { left: 0 },
+  ...buildNumSizes('zIndex', 'z'),
 }
 
 export default function css(className) {
--- src/style.test.js
+++ src/style.test.js
@@ -5,7 +5,10 @@ import css, { styles } from './style'
 it('styles obj creates objects', () => {
   // console.log(styles)
   expect(styles.m1).toEqual({ margin: '1rem' })
-  expect(styles.m)
+  expect(styles.m2).toEqual({ margin: '2rem' })
+  expect(styles.z4).toEqual({ zIndex: 4 })
+  expect(styles.pt0p5).toEqual({ paddingTop: '0.5rem' })
+  expect(styles.pb2).toEqual({ paddingBottom: '2rem' })
 })
 it('creates a merged object from css string', () => {
   expect(css('m0 mt3 pb2')).toEqual({ margin: 0, marginTop: '3rem', paddingBottom: '2rem' })
