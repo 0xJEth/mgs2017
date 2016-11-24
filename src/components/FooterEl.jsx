517986ab72b9dbc178cc26e7df27a9429ff2b359
0xJEth <james.murphy0622@gmail.com>
2016-11-24 00:47:43 -0500

    woohoo

--- src/components/FooterEl.jsx
+++ src/components/FooterEl.jsx
@@ -14,7 +14,7 @@ function FooterEl() {
           </div>
         </section>
         <section className="fifth column">
-          <ul className="list-reset">
+          <ul className="ls-none m0 p0">
             <li><a href="">Office of Graduate Studies</a></li>
             <li>131 West North Avenue</li>
             <li>Baltimore, MD 21201</li>
@@ -22,36 +22,36 @@ function FooterEl() {
           </ul>
         </section>
         <section className="fifth column">
-          <ul className="list-reset">
+          <ul className="ls-none m0 p0">
             <li><a href="https://www.mica.edu/Admission_and_Financial_Aid/Graduate_Admission_and_Financial_Aid.html?utm_source=mica%20grad%20show2017">MICA Graduate Admissions</a></li>
             <li><a href="http://micagradstudies.tumblr.com/?utm_source=mica%20grad%20show2017">MICA Grad Show Blog</a></li>
             <li><a href="http://eepurl.com/bciqMT">Sign up for the MICA Grad Show mailing list</a></li>
-            </ul>
-          </section>
-          <section className="tenth column archive">
-            <h3 className="fs1 m0">Archive</h3>
-            <ul className="list-reset">
-              <li><a href="https://2016.micagradshow.com/?utm_source=mica%20grad%20show2017">2016</a></li>
+          </ul>
+        </section>
+        <section className="tenth column archive">
+          <h3 className="fs1 m0">Archive</h3>
+          <ul className="ls-none m0 p0">
+            <li><a href="https://2016.micagradshow.com/?utm_source=mica%20grad%20show2017">2016</a></li>
             <li><a href="http://2015.micagradshow.com/?utm_source=mica%20grad%20show2017">2015</a></li>
             <li><a href="http://graduate.mica.edu/gradshow/2014/?utm_source=mica%20grad%20show2017">2014</a></li>
             <li><a href="http://graduate.mica.edu/thesis/?utm_source=mica%20grad%20show2017">2013</a></li>
           </ul>
         </section>
         <section className="tenth column social">
-          <ul className="list-reset">
+          <ul className="ls-none m0 p0">
             <li>
               <a href="https://www.instagram.com/micagrad/">
-                <i className="fa fa-instagram fa-2x"></i>
+                <i className="fa fa-instagram fa-2x" />
               </a>
             </li>
             <li>
               <a href="https://www.facebook.com/MICAGradAdmission/">
-                <i className="fa fa-facebook-square fa-2x"></i>
+                <i className="fa fa-facebook-square fa-2x" />
               </a>
             </li>
             <li>
               <a href="https://twitter.com/MICAgrad">
-                <i className="fa fa-twitter fa-2x"></i>
+                <i className="fa fa-twitter fa-2x" />
               </a>
             </li>
           </ul>
