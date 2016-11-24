c305299161a8178f236b91406bcbf5ad910b3178
0xJEth <james.murphy0622@gmail.com>
2016-11-23 23:57:56 -0500

    playing around a little and watching things change

--- src/components/AppEl.jsx
+++ src/components/AppEl.jsx
@@ -5,12 +5,12 @@ import Me from './Me'
 
 function AppEl() {
   return (
-    <div className="App text-center">
-      <div className="App-header">
+    <div className="App text-center pb4">
+      <div className="App-header pt2">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to MGS 2017</h2>
       </div>
-      <p className="App-intro">
+      <p className="App-intro pt4">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
       <Me />
