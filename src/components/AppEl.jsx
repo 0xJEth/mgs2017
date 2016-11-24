9eced33ab452cd7089019193a86c3b5bb39af7db
0xJEth <james.murphy0622@gmail.com>
2016-11-24 10:44:08 -0600

    Inline styles the @kb way.

--- src/components/AppEl.jsx
+++ src/components/AppEl.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import css from '../style'
 import logo from '../logo.svg'
 import './App.css'
 import Me from './Me'
@@ -7,11 +8,11 @@ import Footer from './Footer'
 function AppEl() {
   return (
     <div className="App text-center">
-      <div className="App-header pt2">
+      <div className="App-header" style={css('pt2')}>
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to MGS 2017</h2>
       </div>
-      <p className="App-intro pt4">
+      <p className="App-intro" style={css('pt4')}>
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
       <Me />
