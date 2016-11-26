a88bcfa5ff24b58354b2a56941fedb5660ce2a6c
0xJEth <james.murphy0622@gmail.com>
2016-11-26 17:01:16 -0500

    basic detail view setup

--- src/components/Detail/DetailEl.jsx
+++ src/components/Detail/DetailEl.jsx
@@ -2,34 +2,44 @@ import React, { PropTypes } from 'react'
 import css from '../../style'
 import './Detail.css'
 
-function DetailEl() {
+function DetailEl({ gallery, program, receptionDate, showDate, showName }) {
   return (
-    <detail>
-      <div className="group relative">
-        <div className="six columns" style={css('h100vh')}>
-          <div className="inner-wrap">
-            <h1>Teaching</h1>
-            <p className="dateRange">February 26–March 13, 2017</p>
+    <detail className="text-left">
+      <div className="clearfix relative">
+        <div style={css('h100vh w50 fl')}>
+          <div className="inner" style={css('p2')}>
+            <h1>{ showName }</h1>
+            <p className="dateRange">{ showDate }</p>
             <h2>Reception</h2>
-            <p>Friday, February 26, 5–7 pm</p>
+            <p>{ receptionDate }</p>
             <h2>Includes</h2>
-            <ul>
+            <ul style={css('lsnone m0 p0')}>
               <li>
-                <h3>Teaching, MA</h3>
-                <p>Sheila & Richard Riggs Gallery</p>
+                <h3>{ program }</h3>
+                <p>{ gallery }</p>
               </li>
             </ul>
           </div>
         </div>
-        <div className="six columns relative" style={css('h100vh')}>
+        <div className="relative" style={css('h100vh w50 fl')}>
           <iframe className="absolute map" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB5Ls4YpQRd3Pc6nDpWbc-Hsr0UYT_L90E&q=MICA,Baltimore+MD" />
         </div>
       </div>
     </detail>
   )
 }
-DetailEl.defaultProps = {
-}
 DetailEl.propTypes = {
+  gallery: PropTypes.string.isRequired,
+  program: PropTypes.string.isRequired,
+  receptionDate: PropTypes.string.isRequired,
+  showDate: PropTypes.string.isRequired,
+  showName: PropTypes.string.isRequired,
+}
+DetailEl.defaultProps = {
+  gallery: 'Sheila & Richard Riggs Gallery',
+  program: 'Teaching, MA',
+  receptionDate: 'Friday, February 26, 5–7 pm',
+  showDate: 'February 26–March 13, 2017',
+  showName: 'Teaching',
 }
 export default DetailEl
