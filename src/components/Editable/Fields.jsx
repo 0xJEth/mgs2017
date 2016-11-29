c72f6c10679f028e4943c129bf04f2a8482b6750
0xJEth <james.murphy0622@gmail.com>
2016-11-29 16:49:00 -0600

    Move Fields.jsx

--- src/components/Editable/Fields.jsx
+++ src/components/Editable/Fields.jsx
@@ -2,18 +2,16 @@ import React, { PropTypes } from 'react'
 import { map } from 'lodash'
 import Field from './Field'
 
-function Fields({ editing, entity, fields, open, prefix, title }) {
+function Fields({ entity, fields, prefix, title }) {
   return (
     <div>
-      <h2>{title}</h2>
+      {title && <h2>{title}</h2>}
       <ul className="ls-none m0 p0">
         {map(fields, field => (
           <li key={field.id}>
             <Field
               {...field}
               initialValue={entity[field.id]}
-              isEditing={editing === field.id}
-              open={open}
               prefix={prefix}
             />
           </li>
@@ -24,14 +22,12 @@ function Fields({ editing, entity, fields, open, prefix, title }) {
 }
 
 Fields.propTypes = {
-  editing: PropTypes.string,
   entity: PropTypes.object.isRequired,
   fields: PropTypes.array.isRequired,
-  open: PropTypes.func.isRequired,
   prefix: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
 }
 Fields.defaultProps = {
-  title: 'Edit Profile',
+  title: 'Edit Entity',
 }
 export default Fields
