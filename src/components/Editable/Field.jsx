426d8b49c1e30bb10e2a2a64bd1f628a62fbac19
0xJEth <james.murphy0622@gmail.com>
2016-11-29 14:18:05 -0600

    newer props

--- src/components/Editable/Field.jsx
+++ src/components/Editable/Field.jsx
@@ -9,13 +9,12 @@ import EditField from './EditField'
 // Using this for a typical horizontal editable field.
 // Think of this as the field form container.
 function FieldWrapper(props) {
-  const { emptyText, id, isSaving, type, onSubmit, value } = props
-  const preventClose = !isSaving && props.open
-  const open = preventClose || form.open
+  const { emptyText, id, isOpen, isSaving, type, onSubmit, open, value } = props
   const handleOpen = ary(partial(open, pick(props, 'id', 'initialValue')))
+  const handleSubmit = ary(partial(onSubmit, value))
   return (
-    <FieldGroup {...props}>
-      {!open &&
+    <FieldGroup {...props} onSubmit={handleSubmit}>
+      {(isSaving || !isOpen) &&
         <PreviewText
           editable={!isSaving}
           emptyText={emptyText}
@@ -23,16 +22,13 @@ function FieldWrapper(props) {
           value={value}
         />
       }
-      {open &&
+      {isOpen && !isSaving &&
         <EditField
-          fieldEvent={fieldEvent}
-          formEvent={formEvent}
           defaultValue={value}
           form={form}
           id={id}
           key={id}
           onSubmit={onSubmit}
-          preventClose={preventClose}
           type={type}
         />
       }
@@ -41,11 +37,12 @@ function FieldWrapper(props) {
 }
 
 FieldWrapper.propTypes = {
-  editable: PropTypes.bool.isRequired,
   emptyText: PropTypes.string,
   id: PropTypes.string.isRequired,
+  isOpen: PropTypes.bool,
+  isSaving: PropTypes.bool,
   onSubmit: PropTypes.func,
-  open: PropTypes.bool,
+  open: PropTypes.func.isRequired,
   type: PropTypes.oneOf([
     'email',
     'dateTime',
