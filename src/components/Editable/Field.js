47a4e461a66edd731a0180a6448828a429afdeac
0xJEth <james.murphy0622@gmail.com>
2016-11-29 15:41:06 -0600

    comments on Field.js

--- src/components/Editable/Field.js
+++ src/components/Editable/Field.js
@@ -5,11 +5,14 @@ import Component from './Field.jsx'
 
 // Only deal with the state needed to build Group and View or Edit.
 
+// Define if we should show the (FieldView) preview text/button.
 export function showPreview({ isOpen, isSaving }) {
   return isSaving || !isOpen
 }
+// When showPreview false we want to show Edit.
 export const showEdit = negate(showPreview)
 
+// Add in the props we will pull out and use in Field.jsx component.
 export const getStateProps = flow(getState, fieldState => ({
   isEditable: !fieldState.isSaving,
   showPreview: showPreview(fieldState),
