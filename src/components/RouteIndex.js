57f221c519d3bfcf69587d2022eb59f476fe7970
0xJEth <james.murphy0622@gmail.com>
2016-11-30 08:56:56 -0600

    Move RouteIndex into components

---
+++ src/components/RouteIndex.js
@@ -0,0 +1,13 @@
+import colors from './ColorStripes'
+import details from './Detail/Detail'
+import home from './App'
+import me from './Profile/Me'
+import studentList from './StudentList/StudentList'
+
+export {
+  colors,
+  details,
+  home,
+  me,
+  studentList,
+}
