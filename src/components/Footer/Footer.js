c53fcb09510a877cc8fecf38c336b1d358a06875
0xJEth <james.murphy0622@gmail.com>
2016-11-30 09:22:25 -0600

    add back in footer archive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
-// import { connect } from 'react-redux'
-
+import { connect } from 'react-redux'
+import { property } from 'lodash'
+import { createStructuredSelector } from 'reselect'
 import Component from './FooterEl'
 
-export default Component// connect()(Component)
+export const getState = createStructuredSelector({
+  archive: property('db.archive'),
+})
+
+export default connect(getState)(Component)
--- src/components/Profile/Me.js
+++ src/components/Profile/Me.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { isAuthenticated, selectUser } from 'cape-redux-auth'
 import Component from './Me.jsx'
-import { auth } from '../fire/actions'
+import { auth } from '../../fire/actions'
 
 export const getState = createStructuredSelector({
   isAuthenticated,
