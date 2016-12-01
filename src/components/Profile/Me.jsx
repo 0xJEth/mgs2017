ef44796be1e1d7dfbaf8ef8681182f0586c961b1
0xJEth <james.murphy0622@gmail.com>
2016-12-01 09:33:30 -0600

    style

--- src/components/Profile/Me.jsx
+++ src/components/Profile/Me.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+import css from '../../style'
 import Button from '../Button'
 import Page from '../Page'
 import Profile from './Profile'
@@ -8,7 +9,7 @@ function Me({ auth, isAuthenticated, user }) {
   const { email, name, image } = user
   return (
     <Page id="profile" className="p2 cf">
-      <div className="fl w50 pr1">
+      <div style={css('fl w50 pr1')}>
         <h2>{name}</h2>
         <ul className="ls-none m0 p0">
           <li><img src={image} alt={name} /></li>
