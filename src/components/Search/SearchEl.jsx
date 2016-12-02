30447f38729ad70cd239a51e54a7a4d361333057
0xJEth <james.murphy0622@gmail.com>
2016-12-02 15:54:44 -0600

    prep for state handling

--- src/components/Search/SearchEl.jsx
+++ src/components/Search/SearchEl.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import css from '../../style'
 import './Search.css'
 import Icon from '../Icon'
 
-function SearchEl() {
+function SearchEl({ onChange, value }) {
   return (
     <div>
       <label className="search-wrapper relative" htmlFor="search" style={css('fs1p5 lh3')}>
-        <Icon symbol="search" className="absolute fs-golden left-0 z10" aria-hidden="true" style={css('lh3')} />
-        <input className="search bn bb p0p5 pr2 pl4 z1" />
+        <Icon
+          className="fs-golden left-0"
+          hidden
+          style={css('absolute lh3 z2')}
+          symbol="search"
+        />
+        <input
+          className="search bn bb p0p5 pr2 pl4 z1"
+          onChange={onChange} value={value || ''}
+          type="text"
+        />
       </label>
     </div>
   )
 }
 
 SearchEl.propTypes = {
+  onChange: PropTypes.func,
+  value: PropTypes.string,
 }
 SearchEl.defaultProps = {
 }
