9da96c096292da9e1fd3b815e467900f73278dfd
0xJEth <james.murphy0622@gmail.com>
2016-12-01 20:55:01 -0600

    boxShadow builder function

--- src/styleBuilder.js
+++ src/styleBuilder.js
@@ -1,5 +1,12 @@
 import { curry, flow, keys, map, merge, method, propertyOf, range, zipObject } from 'lodash'
 import { createObj } from 'cape-lodash'
+import tinycolor from 'tinycolor2'
+
+export const grey = tinycolor({ r: 220, g: 220, b: 220 })
+
+export function boxShadow(color = grey) {
+  return { boxShadow: `0 0 .2em 0 ${color.setAlpha(0.2).toRgbString()}` }
+}
 
 // Takes a number or string and appends 'rem' to the end of it.
 // Example: rem(1) === '1rem'
