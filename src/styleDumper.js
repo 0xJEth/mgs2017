e7e5874076feab88a9a335c54f05b21fb66951cd
0xJEth <james.murphy0622@gmail.com>
2016-12-01 22:03:11 -0600

    npm run css

---
+++ src/styleDumper.js
@@ -0,0 +1,10 @@
+import { createMarkupForStyles } from 'react-dom/lib/CSSPropertyOperations'
+import { isFunction, map, omitBy } from 'lodash'
+import fs from 'fs'
+import { styles } from './styleBuilder'
+
+const css = map(omitBy(styles, isFunction), (style, name) =>
+  `.${name} { ${createMarkupForStyles(style)} };`
+)
+css.sort()
+fs.writeFileSync('src/cape.css', css.join('\n'))
