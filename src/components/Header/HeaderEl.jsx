788cf84070f3a5810d75f3e3d172e043caae8fa2
0xJEth <james.murphy0622@gmail.com>
2016-12-02 23:53:27 -0600

    change imgsrc based on sizeId

--- src/components/Header/HeaderEl.jsx
+++ src/components/Header/HeaderEl.jsx
@@ -1,14 +1,20 @@
 import React, { PropTypes } from 'react'
+import mgsBlock from '../../mgs2017LogoBlock.svg'
 import mgsInline from '../../mgs2017LogoInline.svg'
 import css from '../../style'
 import './Header.css'
 import Menu from './Menu'
 
-function HeaderEl({ siteName }) {
+const imgBySizeId = {
+  skinny: mgsBlock,
+  wide: mgsInline,
+}
+
+function HeaderEl({ siteName, sizeId }) {
   return (
     <header id="siteHeader" className="bg-yellow" style={css('p2 mb2')} >
       <nav className="clearfix">
-        <img src={mgsInline} alt={siteName} title={siteName} style={css('mw8 m1 mlrauto block')} />
+        <img src={imgBySizeId[sizeId]} alt={siteName} title={siteName} style={css('mw8 m1 mlrauto block')} />
         <Menu />
       </nav>
     </header>
@@ -16,8 +22,10 @@ function HeaderEl({ siteName }) {
 }
 HeaderEl.propTypes = {
   siteName: PropTypes.string,
+  sizeId: PropTypes.string,
 }
 HeaderEl.defaultProps = {
   siteName: 'MICA Grad Show 2017',
+  sizeId: 'wide',
 }
 export default HeaderEl
