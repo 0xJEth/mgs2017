16cdb3a535325fd3609c13934ed93d64ca24553d
0xJEth <james.murphy0622@gmail.com>
2016-12-03 21:51:21 -0600

    listen to tons of stuff

--- src/fire/storeListener.js
+++ src/fire/storeListener.js
@@ -1,12 +1,12 @@
-import { partial } from 'lodash'
+import { forEach, partial } from 'lodash'
 import { dbChanges, handleAuth, typeLoadWatch } from './handler'
+
+const types = ['ShowGroup', 'Show', 'Program', 'Location', 'Person', 'Student']
 
 export default function storeListener(firebase, store) {
   const loadWatchType = typeLoadWatch(firebase, store)
   firebase.auth.onAuthStateChanged(partial(handleAuth, firebase, store))
   dbChanges(firebase, store)
-  loadWatchType('Person')
-  loadWatchType('Program')
-  loadWatchType('Student')
+  forEach(types, loadWatchType)
   return store
 }
