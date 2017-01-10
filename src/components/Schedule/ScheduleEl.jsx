8069157d91139b00fca5ec2b834c84e0861928a2
0xJEth <james.murphy0622@gmail.com>
2017-01-10 15:40:03 -0500

    temporarily add CP blurb...

--- src/components/Schedule/ScheduleEl.jsx
+++ src/components/Schedule/ScheduleEl.jsx
@@ -5,7 +5,7 @@ import './Schedule.css'
 import Search from '../Search/Search'
 import ShowItem from './ShowGroup'
 
-function ScheduleEl({ showGroups }) {
+function ScheduleEl({ curatorialPracticeBlurb, showGroups }) {
   const { onCampusExhibition, singleDay, cityWide } = showGroups
   return (
     <article id="schedule" style={css('pl1 pr1 pb2')}>
@@ -56,6 +56,9 @@ function ScheduleEl({ showGroups }) {
  */}
       <section style={css('mb3')}>
         <h2 style={css('m0 mt3 bb')}>Curatorial Pratice</h2>
+        <div style={css('mt1 mb1 mw7')}>
+          <p>{curatorialPracticeBlurb}</p>
+        </div>
         <div className="shows item-grid">
           {cityWide &&
             map(cityWide, item => <ShowItem key={item.id} {...item} />)
@@ -66,13 +69,15 @@ function ScheduleEl({ showGroups }) {
     </article>
   )
 }
-ScheduleEl.defaultProps = {
-}
 ScheduleEl.propTypes = {
+  curatorialPracticeBlurb: PropTypes.string,
   showGroups: PropTypes.shape({
     onCampusExhibition: PropTypes.arrayOf(PropTypes.object),
     singleDay: PropTypes.arrayOf(PropTypes.object),
     cityWide: PropTypes.arrayOf(PropTypes.object),
   }),
+}
+ScheduleEl.defaultProps = {
+  curatorialPracticeBlurb: 'Students in MICA’s MFA in Curatorial Practice program complete curatorial projects and collaborate with artists, communities, organizations, and venues. Each student works in a variety of contexts and formats in order to bring contemporary art and culture to new audiences. Visitor hours for individual sites vary. For additional information about partnerships, participating artists, visitor hours, and exhibition programming, visit each venue’s website, micacuratorial.org, or micagradshow.com.',
 }
 export default ScheduleEl
