8335bc51d60bceff9462c1d5f21e21d8df14ddf9
0xJEth <james.murphy0622@gmail.com>
2017-01-13 10:45:12 -0500

    trying to figure some things out

--- src/components/Schedule/ShowGroup.jsx
+++ src/components/Schedule/ShowGroup.jsx
@@ -3,6 +3,7 @@ import { map } from 'lodash'
 import moment from 'moment'
 import css from '../../style'
 import './ShowItem.css'
+import Link from '../Link'
 
 function getShowDate({ startDate, endDate }) {
   if (!startDate) return null
@@ -16,12 +17,12 @@ function getReception({ receptionStart, receptionEnd }) {
   const recEndStr = moment(receptionEnd).utc().format('hA')
   return `${recStartStr}–${recEndStr}`
 }
-function ShowGroup({ program, name, description, ...props }) {
+function ShowGroup({ program, name, description, link, ...props }) {
   const showDate = getShowDate(props)
   const reception = getReception(props)
   return (
     <div className="showItem item">
-      <a href="/details" className="block black" style={css('textReset')}>
+      <a href={link} className="block black" style={css('textReset')}>
         <h1 style={css('m0 mb1')}>{ name }</h1>
         {showDate && <h2 style={css('m0 mb1')} className="dateRange">{showDate}</h2>}
         {reception && <h2 style={css('m0 mt1 mb1')}>Reception: <br />{reception}</h2>}
@@ -32,6 +33,7 @@ function ShowGroup({ program, name, description, ...props }) {
     </div>
   )
 }
+
 ShowGroup.propTypes = {
   // gallery: PropTypes.string.isRequired,
   program: PropTypes.shape({
@@ -40,12 +42,14 @@ ShowGroup.propTypes = {
   receptionEnd: PropTypes.string.isRequired,
   receptionStart: PropTypes.string.isRequired,
   showDate: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 }
 ShowGroup.defaultProps = {
   endDate: '',
   gallery: 'Sheila & Richard Riggs Gallery',
+  link: '/details',
   name: '[name here...]',
   program: { name: 'Teaching, MA' },
   receptionEnd: '',
