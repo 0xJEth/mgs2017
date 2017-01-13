1729478fd997ae9136de4d247ee3c2bc713dcc74
0xJEth <james.murphy0622@gmail.com>
2017-01-13 10:25:04 -0500

    fixing some eslint errors

--- src/components/Blurb/Blurb.jsx
+++ src/components/Blurb/Blurb.jsx
@@ -20,10 +20,12 @@ function Blurb({ description, tagline, siteName, mgsBlock }) {
 Blurb.propTypes = {
   description: PropTypes.array.isRequired,
   tagline: PropTypes.string.isRequired,
+  mgsBlock: PropTypes.string.isRequired,
+  siteName: PropTypes.string.isRequired,
 }
 Blurb.defaultProps = {
   description: [
-    `We invite you to join the next generation of creative thinkers—artists, designers, educators, curators and critics—as they demonstrate the power and possibilities of contemporary art and design.`,
+    `We invite you to join the next generation of creative thinkers—artists, designers, filmmakers, educators, curators and critics—as they demonstrate the power and possibilities of contemporary art and design.`,
     `MICA Grad Show 2017 includes exhibitions and critiques, gallery talks and presentations, public programs, a symposium, and student-curated installations throughout Baltimore city.`,
     `Join us for a season of innovative and inspiring events and exhibitions featuring the culminating work of more than 150 of MICA's graduate students from the College's internationally renowned programs.`,
   ],
