244ef7605c5d373150c50fac41cd48a8c2c88b4c
0xJEth <james.murphy0622@gmail.com>
2017-01-17 01:34:12 -0500

    I know its probably not the best or correct way, but I am trying to learn

--- src/components/Detail/DetailEl.jsx
+++ src/components/Detail/DetailEl.jsx
@@ -1,12 +1,28 @@
 import React, { PropTypes } from 'react'
+import { map } from 'lodash'
 import noop from 'lodash/noop'
+import moment from 'moment'
 import css from '../../style'
 import './Detail.css'
 import Close from '../CloseButton'
 
-function DetailEl({ show }) {
+function getShowDate({ startDate, endDate }) {
+  if (!startDate) return null
+  const startStr = moment(startDate).format('MMMM Do')
+  const endStr = moment(endDate).format('MMMM Do')
+  return `${startStr}–${endStr}`
+}
+function getReception({ receptionStart, receptionEnd }) {
+  if (!receptionStart) return null
+  const recStartStr = moment(receptionStart).utc().format('dddd, MMMM D, h')
+  const recEndStr = moment(receptionEnd).utc().format('hA')
+  return `${recStartStr}–${recEndStr}`
+}
+function DetailEl({ show, detailClose }) {
   if (!show) return <div>Loading</div>
-  const { description, detailClose, gallery, program, receptionDate, showDate, name } = show
+  const { description, program, name, ...props } = show
+  const showDate = getShowDate(props)
+  const reception = getReception(props)
   return (
     <detail>
       <Close onClick={detailClose} style={css('absolute')} />
@@ -16,23 +32,26 @@ function DetailEl({ show }) {
             <h1 style={css('m0')}>{ name }</h1>
             <p className="dateRange" style={css('m0 fs2')}>{ showDate }</p>
             <p className="description">
-              {description}
+              { description }
             </p>
             <h2 style={css('m0 mt2 fs2')}>Reception</h2>
-            <p>{ receptionDate }</p>
+            <p>{ reception }</p>
             <ul style={css('lsNone m0 p0 mt2')}>
               <li>
                 <h3 style={css('m0 mb0p5')}>{ program.id }</h3>
-                <p>{ gallery } » any galleries should show up on the map</p>
+                <p>location.name? » any galleries should show up on the map</p>
               </li>
             </ul>
           </div>
           <div className="studentList" style={css('selfEnd')}>
             <p style={css('m0 mb1')}>list of students — based on the showgroup. Default to alpha order of all students in a particular showGroup (should the student names do anything? or are they just there for reference?)</p>
-            <ul style={css('lsNone m0 p0')}>
+            <ul style={css('lsNone m0 mb1 p0')}>
               <li>
-                <p style={css('m0')}>Generate a list ... Student Name / Program</p>
+                <p style={css('m0')}>Generate a list ... Student Name and their Program? is a program listing useful on this page anywhere???</p>
               </li>
+            </ul>
+            <ul style={css('lsNone m0 p0')}>
+              {program && map(program, (item, key) => <li key={key}>{item.name}</li>)}
             </ul>
           </div>
         </div>
@@ -52,14 +71,15 @@ DetailEl.propTypes = {
     showDate: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
   }),
+  detailClose: PropTypes.func.isRequired,
 }
 DetailEl.defaultProps = {
   description: 'Some things have a little description, that could get printed out here',
-  detailClose: noop,
   gallery: 'Sheila & Richard Riggs Gallery',
   program: 'Teaching, MA',
   receptionDate: 'Friday, February 26, 5–7 pm',
   showDate: 'February 26–March 13, 2017',
   showName: 'Show # / Event Name',
+  detailClose: noop,
 }
 export default DetailEl
