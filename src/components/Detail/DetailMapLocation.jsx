78fe583b201ccabf1ed3c92fd24a5aeb4c4f4ffd
0xJEth <james.murphy0622@gmail.com>
2017-01-19 14:10:44 -0500

    trying to improve the studentList

--- src/components/Detail/DetailMapLocation.jsx
+++ src/components/Detail/DetailMapLocation.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import css from '../../style'
 
-function DetailMapLocation() {
+function DetailMapLocation({ name }) {
   return (
-    <div style={css('p1 br1 bgYellow')}>
-      <p style={css('p0 m0 textCenter')}>A</p>
+    <div style={css('')}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style={{
+          cursor: 'pointer',
+        }}
+      >
+        <path d="M12 2c3.196 0 6 2.618 6 5.602 0 2.238-1.058 3.488-2.66 5.38-1.077 1.275-2.302 2.723-3.34 4.698-1.038-1.976-2.263-3.423-3.34-4.697C7.057 11.09 6 9.84 6 7.603 6 4.617 8.804 2 12 2zm0-2C7.802 0 4 3.403 4 7.602c0 6.243 6.377 6.903 8 16.398 1.623-9.495 8-10.155 8-16.398C20 3.402 16.2 0 12 0zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+      </svg>
     </div>
   )
 }
 
 DetailMapLocation.propTypes = {
+name: PropTypes.string
 }
 DetailMapLocation.defaultProps = {
 }
