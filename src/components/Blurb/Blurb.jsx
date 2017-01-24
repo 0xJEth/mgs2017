366ed6c7983cd20ea7f5ce551fb35a4fef5b71ca
0xJEth <james.murphy0622@gmail.com>
2017-01-24 16:14:59 -0500

    capitalize C

--- src/components/Blurb/Blurb.jsx
+++ src/components/Blurb/Blurb.jsx
@@ -26,7 +26,7 @@ Blurb.propTypes = {
 Blurb.defaultProps = {
   description: [
     `We invite you to join the next generation of creative thinkers—artists, designers, filmmakers, educators, curators and critics—as they demonstrate the power and possibilities of contemporary art and design.`,
-    `MICA Grad Show 2017 includes exhibitions and critiques, gallery talks and presentations, public programs, a symposium, and student-curated installations throughout Baltimore city.`,
+    `MICA Grad Show 2017 includes exhibitions and critiques, gallery talks and presentations, public programs, a symposium, and student-curated installations throughout Baltimore City.`,
     `Join us for a season of innovative and inspiring events and exhibitions featuring the culminating work of more than 150 of MICA's graduate students from the College's internationally renowned programs.`,
   ],
   tagline: 'Meet the next generation of creative thinkers.',
