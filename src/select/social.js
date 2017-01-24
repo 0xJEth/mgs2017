a31aabff1c2c5041e261b0392163c3b65a5d4c55
0xJEth <james.murphy0622@gmail.com>
2017-01-24 16:15:22 -0500

    tumblr blog in a couple places

--- src/select/social.js
+++ src/select/social.js
@@ -5,16 +5,20 @@ export function facebookLink(id) {
   return linkProps('Facebook', `https://www.facebook.com/${id}`, 'facebook-square')
 }
 export function instagramLink(id) {
-  return linkProps('instagram', `https://www.instagram.com/${id}`, 'instagram')
+  return linkProps('Instagram', `https://www.instagram.com/${id}`, 'instagram')
 }
 export function twitterLink(id) {
   return linkProps('Twitter', `https://twitter.com/${id}`, 'twitter')
 }
+export function tumblrLink(id) {
+  return linkProps('Tumblr', `https://${id}.tumblr.com/`, 'tumblr-square')
+}
 
-export function socialLinks({ facebook, instagram, twitter }) {
+export function socialLinks({ facebook, instagram, twitter, tumblr }) {
   const links = []
   if (facebook) links.push(facebookLink(facebook))
   if (instagram) links.push(instagramLink(instagram))
   if (twitter) links.push(twitterLink(twitter))
+  if (tumblr) links.push(tumblrLink(tumblr))
   return links
 }
