7b340b09f7de8c9d41f1f977669e17c4078c1146
0xJEth <james.murphy0622@gmail.com>
2017-01-27 13:26:43 -0600

    Select component

---
+++ src/components/Editable/Select.jsx
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react'
+import { isEmpty, map } from 'lodash'
+
+function Select({ options, value, ...props }) {
+  if (isEmpty(options)) return <div>NO OPTIONS</div>
+  return (
+    <select {...props} value={value}>
+      {map(options, (label, val) =>
+        <option key={val} value={value}>{label}</option>
+      )}
+    </select>
+  )
+}
+
+Select.propTypes = {
+  value: PropTypes.string,
+  onBlur: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.objectOf(PropTypes.string),
+}
+Select.defaultProps = {
+  value: '-',
+}
+export default Select
