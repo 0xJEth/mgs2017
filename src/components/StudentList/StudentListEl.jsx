f3b71e03760d0f93a8f0726d3b034901691b48aa
0xJEth <james.murphy0622@gmail.com>
2017-01-30 10:53:34 -0600

    allow searched students to be array

--- src/components/StudentList/StudentListEl.jsx
+++ src/components/StudentList/StudentListEl.jsx
@@ -35,9 +35,7 @@ function StudentListEl({ programOptions, students }) {
               <span className="show"><strong>Show</strong></span>
               <span className="social" />
             </li>
-            {map(students, item =>
-              <Student key={item.id} {...item} />)
-            }
+            {map(students, item => <Student key={item.id} {...item} />)}
           </ul>
         </section>
       </article>
@@ -46,7 +44,10 @@ function StudentListEl({ programOptions, students }) {
 }
 
 StudentListEl.propTypes = {
-  students: PropTypes.objectOf(PropTypes.object),
+  students: PropTypes.oneOfType([
+    PropTypes.objectOf(PropTypes.object),
+    PropTypes.arrayOf(PropTypes.object),
+  ]),
   programOptions: PropTypes.arrayOf(PropTypes.object),
 }
 StudentListEl.defaultProps = {
