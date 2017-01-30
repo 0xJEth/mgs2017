6821f4069b859a4c9a20cb9c58c00ef9709b78c7
0xJEth <james.murphy0622@gmail.com>
2017-01-30 11:57:03 -0500

    trying to improve detail header situation

--- src/components/FilmFest/FilmFest.jsx
+++ src/components/FilmFest/FilmFest.jsx
@@ -11,55 +11,105 @@ function FilmFest() {
           <p>All film screenings are in Falvey Hall in MICA’s Brown Center.</p>
           <h2>April 28: Friday Night Premieres</h2>
           <ul>
-            <li className="sub-section">4:00pm Photo Wall</li>
-            <li className="sub-section">5:00pm–7:00pm</li>
-            <li className="filmTitle">Mining the Museum</li>
-            <li className="filmTitle">Stranded by Vanity</li>
-            <li className="filmTitle">He’s Gone Now</li>
+            <li className="sub-section">5:00pm Photo Wall & Reception with Light Food and Drink</li>
+            <li className="sub-section">6:00pm–7:00pm</li>
+            <li>
+              <span className="filmTitle">Milo's Misfits</span> by William Bryson / 18-minutes, pilot episode, comedy
+            </li>
+            <li>
+              <span className="filmTitle">He's Gone Now</span> by Jacob Lindsay /  14-minute, comedy
+            </li>
+            <li>
+              <span className="filmTitle">#StrandedByVanity</span> by Janique Robillard / 23-minute Pilot Episode, 2-minute Season Teaser, dark comedy series
+            </li>
             <li className="qa">Q&A with all 3 Filmmakers on Stage</li>
-            <li className="sub-section">8:00pm–10:00pm</li>
-            <li className="filmTitle">Welcome Home Chanel</li>
-            <li className="filmTitle">Milo’s Misfits</li>
-            <li className="filmTitle">The Painting</li>
+            <li className="sub-section">7:30pm–8:30pm</li>
+            <li>
+              <span className="filmTitle">Blue before Gray</span> by Phallon Beckham / 12-minutes, drama, thriller
+            </li>
+            <li>
+              <span className="filmTitle">Welcome Home Chanel</span> by Jenahye' Johnson / 19-minutes, drama
+            </li>
+            <li>
+              <span className="filmTitle">Mining the Museum</span> by Ralph Sporay / 25-minutes, documentary
+            </li>
             <li className="qa">Q&A with all 3 Filmmakers on Stage</li>
           </ul>
         </section>
         <section>
           <h2>April 29: Saturday Night Premieres</h2>
           <ul>
-            <li className="sub-section">4:00pm Photo Wall</li>
-            <li className="sub-section">5:00pm–7:00pm</li>
-            <li className="filmTitle">The Eulogy</li>
-            <li className="filmTitle">PREY</li>
-            <li className="filmTitle">Easter in America</li>
+            <li className="sub-section">5:00pm Photo Wall & Reception with Light Food and Drink</li>
+            <li className="sub-section">6:00pm–6:45pm</li>
+            <li>
+              <span className="filmTitle">All to Myself</span> by Maceo Lester / 7-minutes, drama, music short
+            </li>
+            <li>
+              <span className="filmTitle">The Painting</span> by Andrea Lagos / 15-minutes, horror, arthouse
+            </li>
+            <li>
+              <span className="filmTitle">Telekinesis</span> by James Duffy / 15-minutes, comedy
+            </li>
             <li className="qa">Q&A with all 3 Filmmakers on Stage</li>
-            <li className="sub-section">8:00pm–10:00pm</li>
-            <li className="filmTitle">All to Myself </li>
-            <li className="filmTitle">Telekinesis </li>
-            <li className="filmTitle">I’m Alive and Doing Well</li>
+            <li className="sub-section">7:15pm–8:00pm</li>
+            <li>
+              <span className="filmTitle">The Eulogy</span> by Carlos Harris / 15-minutes, drama
+            </li>
+            <li>
+              <span className="filmTitle">easter in america</span> by Alexander Rubin / 15-minutes, dramedy
+            </li>
+            <li>
+              <span className="filmTitle">I'm Alive & Doing Well</span> by Michael Smigiel / 15-minutes, comedy, road trip, bromancel
+            </li>
             <li className="qa">Q&A with all 3 Filmmakers on Stage</li>
           </ul>
         </section>
         <section>
-          <h2>April 30: Sunday Screenings (No Q&A)</h2>
+          <h2>April 30: Sunday Screenings</h2>
           <ul>
-            <li className="sub-section">11:00–1:00pm</li>
-            <li className="filmTitle">Easter in America</li>
-            <li className="filmTitle">All to Myself </li>
-            <li className="filmTitle">Telekinesis</li>
-            <li className="filmTitle">I’m Alive and Doing Well</li>
+            <li className="sub-section">1:00–2:00pm</li>
+            <li>
+              <span className="filmTitle">#StrandedByVanity</span> by Janique Robillard / 23-minute Pilot Episode, 2-minute Season Teaser, dark comedy series
+            </li>
+            <li>
+              <span className="filmTitle">Milo's Misfits</span> by William Bryson / 18-minutes, Pilot Episode, comedy
+            </li>
+            <li>
+              <span className="filmTitle">He's Gone Now</span> by Jacob Lindsay / 14-minute, comedy
+            </li>
             <li className="sub-section">INTERMISSION</li>
-            <li className="sub-section">2:00–4:00pm</li>
-            <li className="filmTitle">Milo’s Misfits</li>
-            <li className="filmTitle">The Painting</li>
-            <li className="filmTitle">The Eulogy</li>
-            <li className="filmTitle">PREY</li>
+            <li className="sub-section">2:15–3:15pm</li>
+            <li>
+              <span className="filmTitle">Mining the Museum</span> by Ralph Sporay / 25-minutes, documentary
+            </li>
+            <li>
+              <span className="filmTitle">Blue before Gray</span> by Phallon Beckham / 12-minutes, drama, thriller
+            </li>
+            <li>
+              <span className="filmTitle">Welcome Home Chanel</span> by Jenahye' Johnson / 19-minutes, drama
+            </li>
             <li className="sub-section">INTERMISSION</li>
-            <li className="sub-section">5:00–7:00pm</li>
-            <li className="filmTitle">Mining the Museum</li>
-            <li className="filmTitle">He’s Gone Now</li>
-            <li className="filmTitle">Welcome Home Chanel</li>
-            <li className="filmTitle">Stranded by Vanity</li>
+            <li className="sub-section">3:00–3:45pm</li>
+            <li>
+              <span className="filmTitle">Telekinesis</span> by James Duffy / 15-minutes, comedy
+            </li>
+            <li>
+              <span className="filmTitle">All to Myself</span> by Maceo Lester / 7-minutes, drama, music short
+            </li>
+            <li>
+              <span className="filmTitle">The Painting</span> by Andrea Lagos /  15-minutes, horror, arthouse
+            </li>
+            <li className="sub-section">INTERMISSION</li>
+            <li className="sub-section">4:00–5:00pm</li>
+            <li>
+              <span className="filmTitle">I'm Alive & Doing Well</span> by Michael Smigiel / 15-minutes, comedy, road trip, bromance
+            </li>
+            <li>
+              <span className="filmTitle">The Eulogy</span> by Carlos Harris /  15-minutes, drama
+            </li>
+            <li>
+              <span className="filmTitle">easter in america</span> by Alexander Rubin /  15-minutes, dramedy
+            </li>
           </ul>
         </section>
       </div>
