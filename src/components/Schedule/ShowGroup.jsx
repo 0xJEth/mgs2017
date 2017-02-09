eefa762fd84cb748778d9ce14561453a1b90787a
0xJEth <james.murphy0622@gmail.com>
2017-02-09 16:38:56 -0600

    show two receptions

--- src/components/Schedule/ShowGroup.jsx
+++ src/components/Schedule/ShowGroup.jsx
@@ -7,14 +7,29 @@ import './ShowItem.css'
 function getLink({ id, key }) {
   return `/details/${key || id}`
 }
-function ShowGroup({ name, program, reception, showDate, ...props }) {
+
+function Reception({ date, extra }) {
+  return (
+    <h2 style={css('m0 mt1 mb1')}>
+      Reception: <br />
+      {date}
+      {extra && <span><br />{extra}</span>}
+    </h2>
+  )
+}
+Reception.propTypes = {
+  date: PropTypes.string.isRequired,
+  extra: PropTypes.string,
+}
+
+function ShowGroup({ extraChild, name, program, reception, showDate, ...props }) {
   const link = getLink(props)
   return (
     <div className="showItem item">
       <Link href={link} className="block black" style={css('textReset')}>
         <h1 style={css('m0 mb1')}>{ name }</h1>
         {showDate && <h2 style={css('m0 mb1')} className="dateRange">{showDate}</h2>}
-        {reception && <h2 style={css('m0 mt1 mb1')}>Reception: <br />{reception}</h2>}
+        {reception && <Reception date={reception} extra={extraChild && extraChild.reception} />}
         <ul style={css('lsNone m0 p0')}>
           {program && map(program, (item, key) => <li key={key}>{item.name}</li>)}
         </ul>
@@ -24,7 +39,9 @@ function ShowGroup({ name, program, reception, showDate, ...props }) {
 }
 
 ShowGroup.propTypes = {
-  // gallery: PropTypes.string.isRequired,
+  extraChild: PropTypes.shape({
+    reception: PropTypes.string,
+  }),
   program: PropTypes.shape({
     name: PropTypes.string,
   }).isRequired,
