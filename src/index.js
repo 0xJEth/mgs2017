afb2b6e079c76eb6fd91ddd20bc486bfc53564ae
0xJEth <james.murphy0622@gmail.com>
2017-04-20 13:27:07 -0400

    import index css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import init from 'cape-react-app'
 import initialState from './initState'
 import * as RouteIndex from './components/RouteIndex'
+import './index.css'
 // const { store, firebase } =
 init(initialState, RouteIndex)
