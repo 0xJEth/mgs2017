6a68ebd946b6f91ae7bc76ec0b9c684f5bc81479
0xJEth <james.murphy0622@gmail.com>
2017-04-23 12:28:31 -0400

    load full entity to allow display of art on profile

--- src/components/Profile/Me.js
+++ src/components/Profile/Me.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import { property } from 'lodash'
 import { createStructuredSelector } from 'reselect'
+import { fullEntitySelector } from 'redux-graph'
 import { isAnonymous, isAuthenticated, selectUser } from 'cape-redux-auth'
 import { auth } from 'cape-firebase'
 import Component from './Me.jsx'
@@ -13,7 +14,7 @@ export const getState = createStructuredSelector({
   isAnonymous,
   isAuthenticated,
   isStudent,
-  user: selectUser,
+  user: fullEntitySelector(selectUser),
 })
 const actions = { auth }
 export default connect(getState, actions)(Component)
