fe9143733dc0b84b00daf652f3bbd89501a496ba
0xJEth <james.murphy0622@gmail.com>
2017-04-24 02:44:29 -0400

    sorta

--- src/components/Profile/ArtEdit.js
+++ src/components/Profile/ArtEdit.js
@@ -1,4 +1,4 @@
-import { get, sortBy } from 'lodash'
+import { get, map, sortBy } from 'lodash'
 import { connect } from 'react-redux'
 import { structuredSelector } from 'cape-select'
 import { selectGraph, fullEntitySelector } from 'redux-graph'
@@ -8,10 +8,10 @@ import * as entityFields from './artFields'
 // const selectEntity = flow(nthArg(1), property('prefix'), partial(select, selectGraph))
 const selectEntity = (state, { prefix }) => get(selectGraph(state), prefix)
 const selectEntityFull = fullEntitySelector(selectEntity)
-
+const addId = (item, id) => ({ ...item, id, ho: 'l' })
 export const getStateProps = structuredSelector({
   entity: selectEntityFull,
-  fields: sortBy(entityFields, 'position'),
-  title: 'Edit/Add Artwork:',
+  fields: sortBy(map(entityFields, addId), 'position'),
+  title: 'Artwork',
 })
 export default connect(getStateProps)(Component)
--- src/components/Profile/artFields.js
+++ src/components/Profile/artFields.js
@@ -46,6 +46,6 @@ export const image = {
   id: 'image',
   name: 'Image',
   position: 12,
-  single: false,
+  single: true,
   type: 'file',
 }
