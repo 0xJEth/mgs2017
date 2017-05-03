7d547c5e0b281bb105121baa999d5b5277b33632
0xJEth <james.murphy0622@gmail.com>
2017-05-03 16:03:28 -0400

    let cape-mixer find prefix from entity

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,13 +3,13 @@ import { selectUser } from 'cape-redux-auth'
 import Component from 'cape-mixer/lib/Editable/Fields'
 import fields from './personFields'
 
-export const PREFIX = 'profile'
+// export const PREFIX = 'profile'
 // Load the entity we want to edit and the schema defining the fields.
 export function getState(state) {
   return {
     entity: selectUser(state),
     fields,
-    prefix: PREFIX,
+    // prefix: PREFIX,
     title: 'Edit Profile',
   }
 }
