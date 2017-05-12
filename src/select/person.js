6ea9633f743762dfa324460257c848226f35dbcf
0xJEth <james.murphy0622@gmail.com>
2017-05-12 12:44:33 -0400

    calculated fields

--- src/select/person.js
+++ src/select/person.js
@@ -1,26 +1,45 @@
-import { mapValues, merge } from 'lodash'
+import { mapValues, merge, overSome } from 'lodash'
 import { filter, flow, keyBy, map, matchesProperty } from 'lodash/fp'
 import { createSelector } from 'reselect'
-import { setWith } from 'cape-lodash'
+import { setField, setWith } from 'cape-lodash'
 import { entityTypeSelector } from 'redux-graph'
 import { getEmailParts } from './util'
 
+export const getHasDetail = overSome(['statement'])
+export const personFill = flow(
+  setWith('emailParts', 'email', getEmailParts),
+  setField('hasDetail', getHasDetail)
+)
 // Limit to mica.edu people. Key by email username.
 // @TODO will need to add artwork?
-export const personFill = flow(
-  map(setWith('emailParts', 'email', getEmailParts)),
+export const personIndexFill = flow(
+  map(personFill),
   filter(matchesProperty('emailParts[1]', 'mica.edu')),
   keyBy('emailParts[0]')
 )
 
 export const getPerson = createSelector(
   entityTypeSelector('Person'),
-  personFill
+  personIndexFill
 )
+export function getName({ familyName, givenName, name }) {
+  return name || `${givenName} ${familyName}`
+}
+export function getDetailUrl({ hasDetail, id }) {
+  return hasDetail ? `/students/${id}` : null
+}
+export const studentFill = flow(
+  setField('name', getName),
+  setField('detailUrl', getDetailUrl)
+)
+export function studentIndexFill(Student, Person) {
+  return mapValues(Student, item =>
+    studentFill(Person[item.id] ? merge({}, Person[item.id], item) : item)
+  )
+}
 // Person and Student are merged before handling further. Do not worry about Person elsewhere.
 export const getStudent = createSelector(
   entityTypeSelector('Student'),
   getPerson,
-  (Student, Person) => mapValues(Student, item =>
-    (Person[item.id] ? merge({}, Person[item.id], item) : item))
+  studentIndexFill
 )
