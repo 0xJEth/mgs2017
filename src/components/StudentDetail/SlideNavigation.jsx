cfe795446fc45b80bb3ee0e35f9981d5373d71ce
0xJEth <james.murphy0622@gmail.com>
2017-05-16 10:23:09 -0400

    eslint

--- src/components/StudentDetail/SlideNavigation.jsx
+++ src/components/StudentDetail/SlideNavigation.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import css from 'cape-style'
+// import css from 'cape-style'
 
 function slideNavigation({ slideAdvance, slideRewind }) {
   return (
     <ul className="thumbs-navigation">
       <li>
         <button onClick={slideRewind}>
-          <i className="fa fa-chevron-circle-left fa-5x"></i>
+          <i className="fa fa-chevron-circle-left fa-5x" />
         </button>
       </li>
       <li>
         <button onClick={slideAdvance}>
-          <i className="fa fa-chevron-circle-right fa-5x"></i>
+          <i className="fa fa-chevron-circle-right fa-5x" />
         </button>
       </li>
     </ul>
